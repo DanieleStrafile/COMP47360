dc68df75aba3f4c8daef3f6c099fa7c1f7a0c944
Eoin Kenny <eoinmkenny@gmail.com>
2017-08-06 16:52:53 +0100

    Added vizualiztion to map, colour to the 3 routes chosen and info windows to display information

--- FlaskApp/FlaskApp/static/map.js
+++ FlaskApp/FlaskApp/static/map.js
@@ -8,6 +8,10 @@ var busStops = [];
 var waypts = [];
 // For when the user selects a search preference for the map
 var searchPreference;
+// The loading window which also shows the search preferences
+var infoWindow;
+// Information returned from map searches
+var topThreeRoutes;
 
 
 function initialize() {
@@ -22,7 +26,7 @@ function initialize() {
 
 	var source;
 	var destination;
-
+	
 	var map = new google.maps.Map(document.getElementById("googleMap"), myOptions);
 
     // Listener for placing markers
@@ -36,7 +40,24 @@ function initialize() {
 		else if (markersArray.length == 1) {
 			destination = event.latLng;
 			placeMarker(destination, map);
-			getJpidBestRoute(map, source.lat(), source.lng(), destination.lat(), destination.lng());
+			
+			// For the loading icon when a user picks a source and destination
+			infoWindow = new google.maps.InfoWindow({
+				  content: "<h2>Loading...</h2><BR><h3>(can take a while)</h3>"
+				});	
+			
+			infoWindow.setPosition(markersArray[1].center);
+			infoWindow.open(map);
+		
+			setTimeout(function(){// Get data and draw polylines
+			topThreeRoutes = getJpidBestRoute(map, source.lat(), source.lng(), destination.lat(), destination.lng());}, 1000);
+			
+			setTimeout(function(){// Make new infoWindow content
+			infoWindow.setContent(
+			"<h3 style='color:#0014ff;'>" + topThreeRoutes[0][1] + "</h3><b>: " + topThreeRoutes[0][0] + "</b><BR>" + 
+			"<h3 style='color:#ffd800;'>" + topThreeRoutes[1][1] + "</h3><b>: " + topThreeRoutes[1][0] + "</b><BR>" + 
+			"<h3 style='color:#FF0000;'>" + topThreeRoutes[2][1] + "</h3><b>: " + topThreeRoutes[2][0] + "</b>");}, 1000);
+
 		} else {
 
 			// Resetting markers array
@@ -59,6 +80,7 @@ function initialize() {
 				for (var i = 0; i < flightPath.length; i++) {
 					flightPath[i].setMap(null);
 				}
+				flightPath = [];
 			}
 			catch (TypeError) {
 				document.getElementById("displayRouteInNavBarForMap").innerHTML = "<h1 class='customBusHeading'>No Route Found</h1>";
@@ -80,6 +102,9 @@ function placeMarker(location, map) {
             center: location,
             radius: 1000
           });
+	
+	if (markersArray.length == 0) marker.name = "source";
+	if (markersArray.length == 1) marker.name = "destination";
 
 	markersArray.push(marker);
 }
@@ -87,8 +112,13 @@ function placeMarker(location, map) {
 
 //find best possible route jpid, get coords of its stops and display them
 function getJpidBestRoute(map, srcLat, srcLon, destLat, destLon) {
-
-	var jqxhr = $.getJSON( $SCRIPT_ROOT + "/best_route/" + srcLat + "/" + srcLon + "/" + destLat + "/" + destLon, function(data) {
+	
+	
+	$.ajax({
+	  dataType: "json",
+	  url: $SCRIPT_ROOT + "/best_route/" + srcLat + "/" + srcLon + "/" + destLat + "/" + destLon,
+	  async: false, 
+	  success: function(data) {
 
 		// For displaying the route number, or displaying an error
 		var result;
@@ -102,10 +132,11 @@ function getJpidBestRoute(map, srcLat, srcLon, destLat, destLon) {
 			jpid = bestRoutes[i][1];
 			
 			drawMapRoute(map);
-		}
-				
-			
+		}	
+	}
 	});
+		
+	return bestRoutes;
 }
 
 
@@ -208,7 +239,7 @@ function drawMapRoute(map) {
 	var lineSymbol = {
 	  path: google.maps.SymbolPath.CIRCLE,
 	  scale: 8,
-	  strokeColor: '#393'
+	  strokeColor: '#FF0000'
 	};
 
 		  var tempFlightPath = new google.maps.Polyline({
@@ -225,6 +256,12 @@ function drawMapRoute(map) {
 
 		// Add new flightpath to Array
 		flightPath.push(tempFlightPath);
+		// Change colour for each polyline
+		if (flightPath.length == 1) {flightPath[0].strokeColor = '#0014ff';
+									lineSymbol.strokeColor = '#0014ff';}
+		if (flightPath.length == 2) {flightPath[1].strokeColor = '#ffd800';
+									lineSymbol.strokeColor = '#ffd800';}
+		
 		flightPath[flightPath.length - 1].setMap(map);
 		drawBusStops(waypts[waypts.length - 1], map);
 		animateCircle(flightPath[flightPath.length - 1]);
@@ -263,6 +300,11 @@ function drawBusStops(stops, map) {
 			center: stops[i],
 			radius: 50
 		  });
+		
+		if (busStops.length == 1) marker.fillColor = '#0014ff';
+		if (busStops.length == 2) marker.fillColor = '#ffd800';
+		if (busStops.length == 3) marker.fillColor = '#FF0000';
+		
 		// Add the next bus stop to the 2d array
 		busStops[busStops.length - 1].push(marker);
 	  }
